98a3604e9861a6f5f9833d2b34170d349aebd3f0
sani yakubu <ysani095@gmail.com>
2024-07-29 09:43:48 +0100

    updated

--- frontend/src/components/ResultPage.tsx
+++ frontend/src/components/ResultPage.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { useContextStoreProvider } from "../context/store";
 import axios from "axios";
-
+import Ai from "../assets/Ai.png";
+import { AiTwotoneDislike } from "react-icons/ai";
+import { AiTwotoneLike } from "react-icons/ai";
+import { SlLike } from "react-icons/sl";
+import { SlDislike } from "react-icons/sl";
 const ResultPage = () => {
   const {
     setSearchReslt,
@@ -11,7 +15,7 @@ const ResultPage = () => {
     setisloading,
     isLoading,
   } = useContextStoreProvider();
-  const [isReadmore, setIsReadmore] = useState(false);
+  // const [isReadmore, setIsReadmore] = useState(false);
 
   const getData = async () => {
     if (!searchVal) {
@@ -32,10 +36,11 @@ const ResultPage = () => {
       console.log(error);
     }
   };
+  console.log(searchReslt?.organic_results[0]?.link);
 
   return (
     <section className="w-[100%] px-2 md:px-5 md:w-[80%] mx-auto ">
-      <div className="flex justify-between  items-center mx-auto mt-5">
+      <div className="flex items-center justify-between mx-auto mt-5">
         <div className="flex w-[98%] justify-between items-center rounded-l-3xl rounded-r-2xl bg-white md:w-[40%]">
           <input
             value={searchVal as string}
@@ -51,15 +56,48 @@ const ResultPage = () => {
         </div>
         <div className="hidden md:block">
           <div className="avatar placeholder">
-            <div className="bg-neutral text-neutral-content w-10 rounded-full">
+            <div className="w-10 rounded-full bg-neutral text-neutral-content">
               <span className="text-xl">D</span>
             </div>
           </div>
         </div>
       </div>
-      {searchReslt?.answer_box && searchReslt?.answer_box[0] && (
-        <article className="mt-10 lg:w-[60%] bg-slate-300 text-black shadow-md rounded-md p-5">
-          <h1 className=" text-2xl font-bold mb-5 hover:underline">
+      {searchReslt?.organic_results && (
+        <div className="relative h-56 max-w-xl mt-20">
+          <div className="absolute top-0 z-[-2] h-full w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+          <p className="flex items-center mx-5 mb-8 text-xl font-bold text-blue-400">
+            <img src={Ai} alt="Ai" className="object-contain w-[40px] " />
+            智能回答
+          </p>
+          <a
+            className="mx-5 text-xl text-black"
+            target="_blank"
+            href={searchReslt?.organic_results[0]?.link}
+          >
+            {!searchReslt?.organic_results[0]?.title &&
+              "抱歉，出了有点问题，请稍后重试"}
+          </a>
+
+          <p className="w-32 m-5 mx-auto text-center text-white bg-gray-500 rounded-3xl">
+            重新回答
+          </p>
+
+          <div className="absolute bottom-0 flex items-center justify-between w-full">
+            <div className="flex p-2 m-2 bg-gray-600 rounded-3xl gap-x-5">
+              <SlLike className="text-xl text-white " />
+              <SlDislike className="text-xl text-white " />
+            </div>
+            <div className="flex p-2 m-2 bg-gray-600 rounded-3xl gap-x-5">
+              <SlLike className="text-xl text-white " />
+              <SlDislike className="text-xl text-white " />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* {searchReslt?.answer_box && searchReslt?.answer_box[0] && (
+        <article className="lg:w-[60%] bg-slate-300 text-black shadow-md rounded-md p-5">
+          <h1 className="mb-5 text-2xl font-bold hover:underline">
             <a target="_blank" href={searchReslt?.answer_box[0]?.link}>
               {searchReslt?.answer_box[0]?.title}
             </a>
@@ -69,7 +107,7 @@ const ResultPage = () => {
               <p>{searchReslt?.answer_box[0]?.snippet}</p>
               <p
                 onClick={() => setIsReadmore(!isReadmore)}
-                className="cursor-pointer font-bold text-center mt-10"
+                className="mt-10 font-bold text-center cursor-pointer"
               >
                 Read Less
               </p>
@@ -79,42 +117,41 @@ const ResultPage = () => {
               <p>{searchReslt?.answer_box[0]?.snippet.substring(0, 200)}....</p>
               <p
                 onClick={() => setIsReadmore(!isReadmore)}
-                className="cursor-pointer font-bold text-center mt-10"
+                className="mt-10 font-bold text-center cursor-pointer"
               >
                 Read More
               </p>
             </div>
           )}
         </article>
-      )}
+      )} */}
 
-      <div className="flex mt-20 w-full flex-col lg:flex-row justify-between">
-        <article className="flex mt-10 gap-10 flex-col w-[100%] lg:w-[60%] mb-10 pb-5">
+      <div className="flex flex-col justify-between w-full lg:flex-row">
+        <article className="flex gap-10 flex-col w-[100%] lg:w-[60%] mb-10 pb-5">
           {searchReslt?.organic_results?.map((rslt: any) => {
             return (
               <div>
                 <a
-                  className="text-blue-300 mb-10"
+                  className="mb-10 text-blue-300"
                   target="_blank"
                   href={rslt?.link}
                 >
                   {rslt?.title}
                 </a>
 
-                <p className=" mt-3">{rslt?.snippet}</p>
+                <p className="mt-3 ">{rslt?.snippet}</p>
               </div>
             );
           })}
         </article>
-
-        <div className=" flex flex-col gap-10">
+        <div className="flex flex-col gap-10 ">
           {searchReslt?.related_searches && searchReslt?.related_searches && (
-            <div className="mt-10 flex flex-col gap-3">
-              <h1 className=" text-2xl font-bold">Related Search</h1>
+            <div className="flex flex-col gap-3 mt-10">
+              <h1 className="text-2xl font-bold ">Related Search</h1>
               {searchReslt?.related_searches?.map((results: any) => {
                 return (
                   <ul>
-                    <li className=" underline text-blue-400">
+                    <li className="text-blue-400 underline ">
                       <a target="_blank" href={results?.link}>
                         {results?.query}
                       </a>
@@ -126,12 +163,12 @@ const ResultPage = () => {
           )}
           {searchReslt?.people_also_search_for &&
             searchReslt?.people_also_search_for && (
-              <div className="mt-10 flex flex-col gap-3">
-                <h1 className=" text-2xl font-bold">People also search for</h1>
+              <div className="flex flex-col gap-3 mt-10">
+                <h1 className="text-2xl font-bold ">People also search for</h1>
                 {searchReslt?.people_also_search_for?.map((results: any) => {
                   return (
                     <ul>
-                      <li className=" underline text-blue-400">
+                      <li className="text-blue-400 underline ">
                         <a target="_blank" href={results?.link}>
                           {results?.text}
                         </a>
--- frontend/src/components/frontPage.tsx
+++ frontend/src/components/frontPage.tsx
@@ -2,6 +2,9 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useContextStoreProvider } from "../context/store";
+import { FiSearch } from "react-icons/fi";
+import { TbMenuOrder } from "react-icons/tb";
+import { FaArrowRightLong } from "react-icons/fa6";
 const FrontPage = () => {
   const {
     setSearchReslt,
@@ -36,20 +39,80 @@ const FrontPage = () => {
   };
 
   return (
-    <div className="">
-      <div className="mt-52 p-3 flex justify-center rounded-3xl border bg-white w-1/2 mx-auto">
-        <input
-          value={searchVal as string}
-          onChange={(e) => setSearchVal(e.target.value)}
-          name="query"
-          className="w-[89%] outline-none p-2 mx-auto bg-white text-black"
-          placeholder="search..."
-          type="text"
-        />
-        <button onClick={getData} className="btn">
-          search
-          {isLoading && <span className="loading loading-spinner"></span>}
-        </button>
+    <div className="relative flex flex-col items-center justify-center w-full h-screen bg-white">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]" />
+      <div className="absolute py-3 top-0 flex justify-between w-[98%] mx-auto ">
+        <TbMenuOrder className="p-1 text-3xl text-white rounded-full cursor-pointer bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800" />
+        <div>
+          <p className="p-2 text-black border rounded-md cursor-pointer">
+            Feedback
+          </p>
+        </div>
+      </div>
+
+      <div className="w-1/2 mx-auto">
+        <div className="flex flex-col items-center justify-center mb-10">
+          <h1 className="text-4xl font-bold text-black">Search Engine</h1>
+          <p className="text-lg ">
+            Type anything you want to search for and press Enter.
+          </p>
+        </div>
+        <div className="flex justify-between rounded-lg bg-gradient-to-r from-gray-700 via-gray-600 to-gray-800 outline">
+          <input
+            value={searchVal as string}
+            onChange={(e) => setSearchVal(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                getData();
+              }
+            }}
+            name="query"
+            className="flex-1 p-2 mx-auto text-white placeholder-white bg-transparent outline-none"
+            placeholder="Search..."
+            type="text"
+          />
+          <p onClick={getData} className="btn">
+            {!isLoading && <FiSearch className="text-white " />}
+            {isLoading && <span className="loading loading-spinner"></span>}
+          </p>
+        </div>
+        <ul className="mt-8 ">
+          <li
+            onClick={() => setSearchVal("怎么学习react")}
+            className="flex items-center text-xl text-black cursor-pointer gap-x-2 hover:underline"
+          >
+            <span className="text-black ">
+              <FaArrowRightLong />
+            </span>
+            怎么学习react
+          </li>
+          <li
+            onClick={() => setSearchVal("怎么学习python")}
+            className="flex items-center text-xl text-black cursor-pointer gap-x-2 hover:underline"
+          >
+            <span className="text-black ">
+              <FaArrowRightLong />
+            </span>
+            怎么学习python
+          </li>
+          <li
+            onClick={() => setSearchVal("咖啡")}
+            className="flex items-center text-xl text-black cursor-pointer gap-x-2 hover:underline"
+          >
+            <span className="text-black ">
+              <FaArrowRightLong />
+            </span>
+            咖啡
+          </li>
+        </ul>
+      </div>
+      <div className="absolute px-2 mb-2 py-3 bottom-0 flex justify-between w-[98%] mx-auto ">
+        <p>@2024 baidu</p>
+        <ul className="flex gap-x-2">
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Settings</li>
+        </ul>
       </div>
     </div>
   );
